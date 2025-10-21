2e2ea99a62ea3042df4cb6a53d92c53376cb30fd
Dimas Rocha <dimas.dasp@gmail.com>
2025-10-21 00:37:38 -0300

    Controller

--- src/main/java/com/example/kafka/kafka/test/Application.java
+++ src/main/java/com/example/kafka/kafka/test/Application.java
@@ -2,12 +2,34 @@ package com.example.kafka.kafka.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/kafka")
+class HelloController {
+	private HelloProducer service;
+
+	public HelloController(HelloProducer service) {
+
+		this.service = service;
+	}
+
+	@GetMapping("/hello/{name}")
+	public String hello(@PathVariable("name") String name) {
+		service.sendMessage(name);
+		return "OK";
 	}
 
 }
